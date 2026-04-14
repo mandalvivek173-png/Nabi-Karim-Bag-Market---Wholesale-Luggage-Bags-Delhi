90c4d43180c96e49521213d169944d4c05c4c7b3
mandalvivek173-png(GitHUb) <mandalvivek173@gmail.com>
2026-04-14 23:34:57 +0530

    Final fix for spinning issue

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,6 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+// Agar vivek-mandal-db kaam nahi kar raha, to ye default par switch karega
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId || '(default)');
 export const auth = getAuth(app);
 export default app;
